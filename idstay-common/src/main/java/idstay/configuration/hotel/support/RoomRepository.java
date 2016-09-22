80bb5d668a5d6f8c84d38f12787e8e468ba1ff60
@min <minsoo.kim@jkglobe.com>
2016-09-22 19:20:52 +0900

    CrudRepository를 JpaRepository로 변경

--- idstay-common/src/main/java/idstay/configuration/hotel/support/RoomRepository.java
+++ idstay-common/src/main/java/idstay/configuration/hotel/support/RoomRepository.java
@@ -1,7 +1,7 @@
 package idstay.configuration.hotel.support;
 
 import idstay.configuration.hotel.Room;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends CrudRepository <Room, Long>{
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
