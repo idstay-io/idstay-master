9148f835af9ae1e1118eacf31dea8c382d2d06d5
@min <minsoo.kim@jkglobe.com>
2016-09-23 18:33:31 +0900

    package name refactoring

--- idstay-common/src/main/java/idstay/hotelconfig/crew/support/CrewRepository.java
+++ idstay-common/src/main/java/idstay/hotelconfig/crew/support/CrewRepository.java
@@ -1,13 +1,16 @@
-package idstay.configuration.crew.support;
+package idstay.hotelconfig.crew.support;
 
-import idstay.configuration.crew.Crew;
+
+import idstay.hotelconfig.crew.Crew;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CrewRepository extends CrudRepository<Crew, Long> {
+public interface CrewRepository extends JpaRepository<Crew, Long> {
     Crew findByCrewName(String crewName);
+
     @Query("SELECT c FROM Crew c WHERE c.activate = true")
     List<Crew> findAllCrews();
 }
